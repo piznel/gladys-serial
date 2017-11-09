a38be23b296abfd5e91e954a8b3f31474feab6a1
isokar <matthieu_lemaitre@yahoo.com>
2017-11-09 16:55:46 +0100

    correction of exec absence

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ module.exports = function(sails) {
     var setup = require('./lib/setup.js');
     var connect = require('./lib/connect.js');
     var sendCode = require('./lib/sendCode.js');
-	var exec = require('./lib/exec.js');
 
     gladys.on('ready', function(){
         connect();
@@ -14,7 +13,6 @@ module.exports = function(sails) {
     return {
         connect: connect,
         setup: setup,
-        sendCode: sendCode,
-		exec: exec
+        sendCode: sendCode
     };
 };
